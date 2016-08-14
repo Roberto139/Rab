37edb7cc9009869cc30f682af32521019871485d
Roberto Arruda Braga <rab.arr.bra@gmail.com>
2016-08-13 21:16:57 -0300

    testando entrada de dados

---
+++ entradanome.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char a,b,c,d,e,f;
+    int i,j;
+
+    printf("digite 3 letras");
+    a=getchar();
+    b=getchar();
+    c=getchar();
+
+    printf("%c%c%c \n ", a,b,c);
+
+    return 0;
+}
+
