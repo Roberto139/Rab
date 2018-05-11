76e6e08609491251271f0edfe5083e00f32cd5a3
Roberto Arruda Braga <rob.arr.bra@gmail.com>
2018-05-11 12:02:56 -0300

    Adicioanndo as bibliotecas

---
+++ ex7.h
@@ -0,0 +1,166 @@
+/***************************************************************************
+ *   exN.h                                 Version 20160618.013412        *
+ *                                                                         *
+ *   Converte AFND para AFD                                                *
+ *   Copyright (C) 2016         by Ruben Carlo Benante                     *
+ ***************************************************************************
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License.        *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************
+ *   To contact the author, please write to:                               *
+ *   Ruben Carlo Benante                                                   *
+ *   Email: rcb@beco.cc                                                    *
+ *   Webpage: www.beco.cc                                                  *
+ *   Phone: +55 (81) 3184-7555                                             *
+ ***************************************************************************/
+
+/* ---------------------------------------------------------------------- */
+/**
+ * \file exN.h
+ * \ingroup GroupUnique
+ * \brief Converte AFND para AFD
+ * \details This program really do a nice job as a template, and template only!
+ * \version 20160618.013412
+ * \date 2016-06-18
+ * \author Ruben Carlo Benante <<rcb@beco.cc>>
+ * \par Webpage
+ * <<a href="www.beco.cc">www.beco.cc</a>>
+ * \copyright (c) 2016 GNU GPL v2
+ * \note This program is free software: you can redistribute it
+ * and/or modify it under the terms of the
+ * GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.
+ * If not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA. 02111-1307, USA.
+ * Or read it online at <<http://www.gnu.org/licenses/>>.
+ *
+ *
+ * \todo Now that you have the template, hands on! Programme!
+ * \warning Be carefull not to lose your mind in small things.
+ * \bug This file right now does nothing usefull
+ *
+ */
+
+#ifndef _EX12_H
+#define _EX12_H
+
+/* ---------------------------------------------------------------------- */
+/* includes */
+
+#include <stdio.h> /* Standard I/O functions */
+#include <stdlib.h> /* Miscellaneous functions (rand, malloc, srand)*/
+#include <getopt.h> /* get options from system argc/argv */
+
+/* #include <malloc.h> */ /* Dynamic memory allocation */
+/* #include <time.h> */ /* Time and date functions */
+/* #include <math.h> */ /* Mathematics functions */
+/* #include <string.h> */ /* Strings functions definitions */
+/* #include <dlfcn.h> */ /* Dynamic library */
+/* #include <unistd.h> */ /* UNIX standard function */
+/* #include <limits.h> */ /* Various C limits */
+/* #include <ctype.h> */ /* Character functions */
+/* #include <errno.h> */ /* Error number codes errno */
+/* #include <signal.h> */ /* Signal processing */
+/* #include <stdarg.h> */ /* Functions with variable arguments */
+/* #include <pthread.h> */ /* Parallel programming with threads */
+/* #include <fcntl.h> */ /* File control definitions */
+/* #include <termios.h> */ /* Terminal I/O definitions POSIX */
+/* #include <sys/stat.h> */ /* File status and information */
+/* #include <float.h> */ /* Float constants to help portability */
+/* #include <setjmp.h> */ /* Bypass standard function calls and return */
+/* #include <stddef.h> */ /* Various types and MACROS */
+/* #include <SWI-Prolog.h> */ /* Prolog integration with C */
+/* #include <ncurses.h> */ /* Screen handling and optimisation functions */
+/* #include <allegro.h> */ /* A game library for graphics, sounds, etc. */
+/* #include <libintl.h> */ /* Internationalization / translation */
+/* #include <locale.h> */ /* MACROS LC_ for location specific settings */
+
+/* ---------------------------------------------------------------------- */
+/* definitions */
+
+#ifndef VERSION /* gcc -DVERSION="0.1.160612.142628" */
+#define VERSION "20160618.013412" /**< Version Number (string) */
+#endif
+
+/* Debug */
+#ifndef DEBUG /* gcc -DDEBUG=1 */
+#define DEBUG 0 /**< Activate/deactivate debug mode */
+#endif
+
+#if DEBUG==0
+#define NDEBUG
+#endif
+#include <assert.h> /* Verify assumptions with assert. Turn off with #define NDEBUG */ 
+
+/** @brief Debug message if DEBUG on */
+#define IFDEBUG(M) if(DEBUG) fprintf(stderr, "[DEBUG file:%s line:%d]: " M "\n", __FILE__, __LINE__); else {;}
+
+/* limits */
+#define SBUFF 256 /**< String buffer */
+
+typedef struct st_listaE
+{
+    int e;
+    struct st_listaE *prox;
+}listaE_t;
+
+typedef struct st_listaD
+{
+    int ei;
+    char lei;
+    int ef;
+    struct st_listaD *prox;
+}listaD_t;
+
+typedef struct quintupla
+{
+    int K;
+    char A;
+    int s;
+    struct st_listaE *f;
+    struct st_listaD *D;
+}quint_t;
+
+typedef struct st_llista
+{
+    struct st_listaE *est;
+    struct st_llista *prox;
+}llista_t;
+
+
+/* ---------------------------------------------------------------------- */
+/* globals */
+
+static int verb = 0; /**< verbose level, global within the file */
+
+/* ---------------------------------------------------------------------- */
+/* prototypes */
+
+void help(void); /**< Prints help information and exit */
+void copyr(void); /**< Prints copyright information and exit */
+void exN_init(void);  /**< Initializes some operations before start */
+void exN_update(void); /**< Updates values during program cycle */
+
+#endif /* NOT def _EX12_H */
+
+/* ---------------------------------------------------------------------- */
+/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline */
+/* Template by Dr. Beco <rcb at beco dot cc> Version 20160615.020326      */
+
